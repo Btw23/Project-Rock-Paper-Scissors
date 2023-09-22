406a0ec373766e088e96b8d679577de4333062c1
Btw23 <141656079+Btw23@users.noreply.github.com>
2023-09-22 11:34:28 +0200

    Make function that plays one round of rock, paper, scissors

--- javascript.js
+++ javascript.js
@@ -9,3 +9,34 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+
+// function to play one round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+  playerSelection = prompt("Choose rock, paper or scissors");
+  if (playerSelection === "rock" && computerSelection === "scissors" ||
+      playerSelection === "paper" && computerSelection === "rock" ||
+      playerSelection === "scissors" && computerSelection === "paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+
+      } else if (playerSelection === "rock" && computerSelection === "paper" ||
+                playerSelection === "paper" && computerSelection === "scissors" ||
+                playerSelection === "scissors" && computerSelection === "rock") {
+                  return `You Lose! ${computerSelection} beats ${playerSelection}`;
+                  
+                } else if (playerSelection === "rock" && computerSelection === "rock" ||
+                          playerSelection === "paper" && computerSelection === "paper" ||
+                          playerSelection === "scissors" && computerSelection === "scissors") {
+                            return "Tie!"
+
+                } else {
+                  return "Wrong Input!"
+                }
+}
+
+let computerSelection
+let playerSelection 
+
+
+
