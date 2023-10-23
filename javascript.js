08112ef0624062d7adbccd42e4a13c87a1970c3a
Btw23 <141656079+Btw23@users.noreply.github.com>
2023-10-23 19:18:09 +0200

    Display the score in the div and reset the game after one player wins

--- javascript.js
+++ javascript.js
@@ -14,40 +14,64 @@ function getComputerChoice() {
 // function to play one round of rock, paper, scissors
 function playRound(playerSelection, computerSelection) {
   computerSelection = getComputerChoice();
+  scoreDiv.style.color = "white";
 
   if (playerSelection === "rock" && computerSelection === "scissors" ||
       playerSelection === "paper" && computerSelection === "rock" ||
       playerSelection === "scissors" && computerSelection === "paper") {
-        scoreDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-        return "playerWin";
+        winnerDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++
+        scoreDiv.textContent = `${playerScore} : ${botScore}`;
 
       } else if (playerSelection === "rock" && computerSelection === "paper" ||
                 playerSelection === "paper" && computerSelection === "scissors" ||
                 playerSelection === "scissors" && computerSelection === "rock") {
-                  scoreDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-                  return "botWin"
+                  winnerDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                  botScore++
+                  scoreDiv.textContent = `${playerScore} : ${botScore}`;
                   
                 } else if (playerSelection === "rock" && computerSelection === "rock" ||
                           playerSelection === "paper" && computerSelection === "paper" ||
                           playerSelection === "scissors" && computerSelection === "scissors") {
-                            scoreDiv.textContent = "Tie!";
+                            winnerDiv.textContent = "Tie!";
+                            scoreDiv.textContent = `${playerScore} : ${botScore}`;
+                }
 
+                if (playerScore >= 5) {
+                  scoreDiv.textContent = "You Win!";
+                  scoreDiv.style.color = "green";
+                  playerScore = 0;
+                  botScore = 0;
+
+                } else if (botScore >= 5) {
+                  scoreDiv.textContent = "You Lose!";
+                  scoreDiv.style.color = "red";
+                  playerScore = 0;
+                  botScore = 0;
                 }
 }
 
-let playerSelection;
-let playerScore;
-let botScore;
+let playerScore = 0;
+let botScore = 0;
 
+const winnerDiv = document.querySelector(".winner");
 const scoreDiv = document.querySelector(".score");
 
 const rockButton = document.querySelector(".button-container :nth-child(1)")
 const paperButton = document.querySelector(".button-container :nth-child(2)")
 const scissorsButton = document.querySelector(".button-container :nth-child(3)")
 
-rockButton.addEventListener("click", () => playRound("rock"));
-paperButton.addEventListener("click", () => playRound("paper"));
-scissorsButton.addEventListener("click", () => playRound("scissors"));
+rockButton.addEventListener("click", () => {
+  playRound("rock")
+})
+
+paperButton.addEventListener("click", () => {
+  playRound("paper")
+});
+
+scissorsButton.addEventListener("click", () => {
+  playRound("scissors")
+});
 
 
 
